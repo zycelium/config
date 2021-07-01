aacab16f355283da7a9e7ddf19b898c61824aac9
Harshad Sharma <harshad@sharma.io>
2021-07-01 18:19:32 +0530

    Update test to check if wrapper works.

--- tests/test_dataconfig.py
+++ tests/test_dataconfig.py
@@ -1,5 +1,11 @@
+from dataclasses import dataclass, is_dataclass
 from zycelium.dataconfig import dataconfig
 
 
 def test_dataconfig_init():
-    pass
+    @dataconfig
+    class Config:
+        pass
+
+    config = Config()
+    assert is_dataclass(config)
