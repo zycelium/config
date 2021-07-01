e074f5fa5e04b928c374c46a9d9de3f227e480f0
Harshad Sharma <harshad@sharma.io>
2021-07-01 18:23:12 +0530

    Return dataclass when wrapped with dataconfig.

--- src/zycelium/dataconfig/__init__.py
+++ src/zycelium/dataconfig/__init__.py
@@ -1,2 +1,9 @@
-def dataconfig():
-    pass
+from dataclasses import dataclass
+
+
+def dataconfig(_cls):
+    def wrap():
+        wrapped_cls = dataclass(_cls)
+        return _cls
+
+    return wrap
