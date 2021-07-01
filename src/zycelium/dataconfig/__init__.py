e6995bc7563c48309351f1594b78a71c65924cd7
Harshad Sharma <harshad@sharma.io>
2021-07-01 18:34:52 +0530

    Add "paths" kwarg to dataconfig.

--- src/zycelium/dataconfig/__init__.py
+++ src/zycelium/dataconfig/__init__.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 
 
-def dataconfig(_cls=None, *, file="config.ini"):
+def dataconfig(_cls=None, *, file="config.ini", paths=None):
     def wrap(cls):
         setattr(cls, "_file", file)
+        setattr(cls, "_paths", paths)
         wrapped_cls = dataclass(cls)
         return wrapped_cls
 
--- tests/test_dataconfig.py
+++ tests/test_dataconfig.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, is_dataclass
 from zycelium.dataconfig import dataconfig
 
 TEST_FILE_NAME = "test.ini"
+DEFAULT_FILE_NAME = "config.ini"
 
 
 def test_dataconfig_init_bare():
@@ -11,7 +12,7 @@ def test_dataconfig_init_bare():
 
     config = Config()
     assert is_dataclass(config)
-    assert config._file == "config.ini"
+    assert config._file == DEFAULT_FILE_NAME
 
 
 def test_dataconfig_init_with_file():
@@ -30,5 +31,13 @@ def test_dataconfig_init_without_file():
         pass
 
     config = Config()
-    assert is_dataclass(config)
-    assert config._file == "config.ini"
+    assert config._file == DEFAULT_FILE_NAME
+
+
+def test_dataconfig_init_with_paths():
+    @dataconfig(paths=[".", "tests/"])
+    class Config:
+        pass
+
+    config = Config()
+    assert config._paths == [".", "tests/"]
